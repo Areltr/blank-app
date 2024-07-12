335196162d36ad3b9ed6b062b40ef7cab003f755
Areltr <arelanjay528@gmail.com>
2024-07-12 18:06:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ alas = st.number_input("Masukan Alas", 0)
 tinggi = st.number_input("Masukan Tinggi", 0)
 hitung = st.button("Hitung Luas")
 if hitung:
-    luas = 0.5 * alas * tingg
+    luas = 0.5 * alas * tinggi
     st.success(f"Luas Segitiganya Adalah {luas}")
     st.balloons()
 
