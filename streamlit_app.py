c5a0f495e22c95d6358f333adc54d5af38e8b6a1
Areltr <arelanjay528@gmail.com>
2024-07-12 17:19:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,21 +14,14 @@ if hitung:
     st.success(f"Luas Segitiganya Adalah {luas}")
     st.balloons()
 
-def hitung_tekanan(h, rho, g):
-    tekanan = rho * g * h
-    return tekanan
-
-# Judul dan deskripsi aplikasi
-st.title('Kalkulator Tekanan Hidrostatis')
 st.write('Aplikasi ini menghitung tekanan hidrostatis di dalam fluida.')
 
-# Input dari pengguna
 h = st.number_input('Kedalaman Fluida (meter)', min_value=0.0, step=0.1, format="%.2f")
 rho = st.number_input('Massa Jenis Fluida (kg/m^3)', min_value=0.0, step=1.0, format="%.1f")
 g = st.number_input('Percepatan Gravitasi (m/s^2)', min_value=0.0, step=1.0, format="%.1f", value=9.81)
+hitungtekanan = st.button("Hitung Tekanan")
 
-# Tombol untuk menghitung tekanan
-if st.button('Hitung Tekanan'):
-    tekanan = hitung_tekanan(h, rho, g)
+if hitungtekanan:
+    tekanan = rho * g * h
     st.write(f'Tekanan Hidrostatis = {tekanan:.2f} Pascal')
 
