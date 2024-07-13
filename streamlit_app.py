726944e339410371e54e5961503a04c661ce2486
Areltr <arelanjay528@gmail.com>
2024-07-13 08:15:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,11 @@ st.set_page_config(
     )
 st.sidebar.success("test")
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
